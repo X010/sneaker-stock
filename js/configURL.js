4c3ca3f3f257de46d19bc605bc36cbc863c69057
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-07-03 14:01:18 +0800

    edit url

--- js/configURL.js
+++ js/configURL.js
@@ -1,3 +1,12 @@
+/**
+* @Author: jeffrey
+* @Date:   2016-06-27T19:05:07+08:00
+* @Last modified by:   jeffrey
+* @Last modified time: 2016-07-03T14:00:38+08:00
+*/
+
+
+
 //URL配置模块
 
 //切换环境
@@ -8,7 +17,7 @@ var companySearchUrl;
 switch (ENV){
     case 'P':
         //sneaker API
-        SITE_URL = 'http://yc.api.ms9d.com';
+        SITE_URL = 'http://local.api.test.ms9d.com';
         //搜索商品服务器的URL
         //searchUrl = "http://115.28.93.117:8081/search/goods?index_name=index_goods&type_name=index_goods&page=0&page_num=13&dsl=";
         //搜索公司服务器的URL
@@ -28,4 +37,3 @@ switch (ENV){
         //companySearchUrl = "http://115.28.93.117:8081/search/company?index_name=index_company&type_name=index_company&page=0&page_num=13&dsl=name:";
         break;
 }
-
